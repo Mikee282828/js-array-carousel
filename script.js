fd746e2409047d4912e3b59a5fb5a9891d68ebdb
MikeDesktop <micheledong2803@gmail.com>
2024-04-23 17:20:46 +0200

    m3 - tasto precedente

--- script.js
+++ script.js
@@ -2,24 +2,32 @@ let arrayFoto = document.querySelectorAll(".foto");
 let fotoAttiva;
 let fotoSuccessiva;
 let fotoPrecedente;
-//stampo in pagina l'array foto per controllo
-console.log(arrayFoto);
+const tastoSuccessivo = document.querySelector(".successivo");
+const tastoPrecedente = document.querySelector(".precedente");
 
-//cerco la foto attualmente attiva
-for (let i = 0; i < arrayFoto.length; i++) {
-    if(arrayFoto[i].classList.contains("active")){
-        fotoAttiva = i;
-        arrayFoto[i].classList.remove("active");
+tastoPrecedente.addEventListener("click", function () {
+    //stampo in pagina l'array foto per controllo
+    console.log(arrayFoto);
+
+    //cerco la foto attualmente attiva
+    for (let i = 0; i < arrayFoto.length; i++) {
+        if (arrayFoto[i].classList.contains("active")) {
+            fotoAttiva = i;
+            arrayFoto[i].classList.remove("active");
+        }
     }
-}
-//cerco l'indice della foto successiva e della foto precedente
-if(fotoAttiva+1 == arrayFoto.length){
-    fotoSuccessiva = 0;
-}else{
-    fotoSuccessiva = fotoAttiva + 1;
-}
-if(fotoAttiva == 0){
-    fotoPrecedente = arrayFoto.length - 1;
-}else{
-    fotoPrecedente = fotoAttiva - 1;
-}
+    //cerco l'indice della foto precedente
+    if (fotoAttiva + 1 == arrayFoto.length) {
+        fotoSuccessiva = 0;
+    } else {
+        fotoSuccessiva = fotoAttiva + 1;
+    }
+    if (fotoAttiva == 0) {
+        fotoPrecedente = arrayFoto.length - 1;
+    } else {
+        fotoPrecedente = fotoAttiva - 1;
+    }
+
+    //attivo la foto precedente
+    arrayFoto[fotoPrecedente].classList.add("active");
+})
